16815f31289692f9560a08e3a09b34d8b48db6cd
Curt J. Sampson <cjs@cynic.net>
2018-10-06 13:22:19 +0900

    t/functional: Demonstrate use of Popen of ssh in a test

--- t/functional.py
+++ t/functional.py
@@ -1,4 +1,19 @@
 'Functional Tests'
 
-def test_nothing():
-    assert 1
+import os, re
+from subprocess import Popen, PIPE
+
+#   Global configuration for all functional tests.
+os.environ.pop('SSH_AUTH_SOCK', None)
+DEVNULL = open(os.devnull, 'rwb')
+
+def test_global_config():
+    assert None is os.environ.get('SSH_AUTH_SOCK')
+
+def test_popen_ssh():
+    'Demonstrate use of Popen in a test.'
+    p = Popen(['ssh', '-n', '-h'], stdin=DEVNULL, stdout=PIPE, stderr=PIPE,
+        env={}, bufsize=-1, close_fds=True)
+    assert 0 <= p.wait()            # Any retval but not terminated by a signal.
+    assert '' == p.stdout.read()    # XXX should close these?
+    assert re.search('usage: ssh', p.stderr.read())
